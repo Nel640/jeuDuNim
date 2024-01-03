d8ea399dc03dc2ee2adda3b2361fd4877a2bba2e
Donovan Seulin <nel640@hotmail.fr>
2024-01-03 11:47:33 +0100

    corrections boucle joueur perdu

--- src/JeuDuNim.java
+++ src/JeuDuNim.java
@@ -9,26 +9,36 @@ public class JeuDuNim {
 		String playerOne = scan.nextLine();
 		System.out.println("joueur 2 quel est votre nom ? :");
 		String playerTwo = scan.nextLine();
-		System.out.println("quel joueur commence ? : (1 ou 2)");
-		int playerFirst = scan.nextInt();
 		
 		int candles = 21;
+		String lastPlayer = null;
 		
 		while (candles > 0)
 		{
-			if(playerFirst == 1)
+			System.out.println(playerOne + " combien d'allumette voulez vous retirer entre 1 et 4 ? : ");
+			int pOneRemove = scan.nextInt();
+			candles -= pOneRemove;
+			System.out.println("reste : " + candles + " allumette");
+			if(candles == 1)
 			{
-				System.out.println(playerOne + " combien d'allumette voulez vous retirer entre 1 et 4 ? : ");
-				int pOneRemove = scan.nextInt();
-				candles -= pOneRemove;
+				lastPlayer = playerTwo;
 			}
-			else if(playerFirst == 2)
+			if(candles == 0)
 			{
-				System.out.println(playerTwo + " combien d'allumette voulez vous retirer entre 1 et 4 ? : ");
-				int pTwoRemove = scan.nextInt();
-				candles -= pTwoRemove;
+				break;
+			}
+		
+			System.out.println(playerTwo + " combien d'allumette voulez vous retirer entre 1 et 4 ? : ");
+			int pTwoRemove = scan.nextInt();
+			candles -= pTwoRemove;
+			System.out.println("reste : " + candles + " allumette");
+			if(candles == 1)
+			{
+				lastPlayer = playerOne;
 			}
 		}
+		scan.close();
+		System.out.println(lastPlayer + " a perdu ");
 	}
 
 }
