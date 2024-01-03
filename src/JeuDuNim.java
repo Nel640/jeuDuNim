86318c309a42833eeb34fb6486c00100db3f4b28
Donovan Seulin <nel640@hotmail.fr>
2024-01-03 11:06:51 +0100

    questions reponses aux joueurs

--- src/JeuDuNim.java
+++ src/JeuDuNim.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class JeuDuNim {
 
 	public static void main(String[] args) {
-		System.out.println("ca marche aussi");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("joueur 1 quel est votre nom ? :");
+		String playerOne = scan.nextLine();
+		System.out.println("joueur 2 quel est votre nom ? :");
+		String playerTwo = scan.nextLine();
+		System.out.println("quel joueur commence ? : (1 ou 2)");
+		int playerFirst = scan.nextInt();
+		
 
 	}
 
